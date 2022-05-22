58d66715340618246d0d1fc18a3dcb75d52e97d5
unknown <you@example.com>
2022-05-22 01:20:37 -0400

    added shipment details to server checkout session

--- server.js
+++ server.js
@@ -21,6 +21,53 @@ app.post("/create_checkout_session", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types:['card'],
     mode:'payment',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'usd',
+          },
+          display_name: 'Free shipping',
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 5,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 7,
+            },
+          }
+        }
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 1500,
+            currency: 'usd',
+          },
+          display_name: 'Next day air',
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 1,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 1,
+            },
+          }
+        }
+      },
+    ],
     line_items:req.body.cart.map((item) => {
 
       return{

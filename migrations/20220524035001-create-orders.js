8c95e1c564322070e114ffb43173962e5ab9cf42
unknown <you@example.com>
2022-05-23 23:53:05 -0400

    added tables to db

---
+++ migrations/20220524035001-create-orders.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      customerName: {
+        type: Sequelize.STRING
+      },
+      customerAddress: {
+        type: Sequelize.STRING
+      },
+      cart: {
+        type: Sequelize.ARRAY
+      },
+      total: {
+        type: Sequelize.INTEGER
+      },
+      paymentSuccesful: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('orders');
+  }
+};
---
+++ models/orders.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class orders extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  orders.init({
+    customerName: DataTypes.STRING,
+    customerAddress: DataTypes.STRING,
+    cart: DataTypes.ARRAY,
+    total: DataTypes.INTEGER,
+    paymentSuccesful: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'orders',
+  });
+  return orders;
+};

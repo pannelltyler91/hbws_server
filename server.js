d954fa2bf0dc3ba0022ccab59b22586c7d97bd0e
unknown <you@example.com>
2022-05-30 23:01:14 -0400

    updated server to start getting ready to create order in database

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4242;
 const db = require("./models");
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_409438078d23c5dc7bcad2c0b72883cfd2a156054710a9f3c2e1980aa45c3d1f";
 
 app.use(cors());
 app.use(express.json());
@@ -17,80 +20,121 @@ app.use(
 
 //add async back in before req, res
 app.post("/create_checkout_session", async (req, res) => {
-    try{
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types:['card'],
-    mode:'payment',
-    shipping_address_collection: {
-      allowed_countries: ['US', 'CA'],
-    },
-    shipping_options: [
-      {
-        shipping_rate_data: {
-          type: 'fixed_amount',
-          fixed_amount: {
-            amount: 0,
-            currency: 'usd',
-          },
-          display_name: 'Free shipping',
-          // Delivers between 5-7 business days
-          delivery_estimate: {
-            minimum: {
-              unit: 'business_day',
-              value: 5,
-            },
-            maximum: {
-              unit: 'business_day',
-              value: 7,
-            },
-          }
-        }
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
       },
-      {
-        shipping_rate_data: {
-          type: 'fixed_amount',
-          fixed_amount: {
-            amount: 1500,
-            currency: 'usd',
-          },
-          display_name: 'Next day air',
-          // Delivers in exactly 1 business day
-          delivery_estimate: {
-            minimum: {
-              unit: 'business_day',
-              value: 1,
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "usd",
             },
-            maximum: {
-              unit: 'business_day',
-              value: 1,
+            display_name: "Free shipping",
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
             },
-          }
-        }
-      },
-    ],
-    line_items:req.body.cart.map((item) => {
-
-      return{
-        price_data:{
-          currency:'usd',
-          product_data:{
-            name:item.name
           },
-          unit_amount:item.price * 100
         },
-        quantity:item.quantity
-      }
-    }),
-    success_url:`${process.env.SERVER_URL}/success`,
-    cancel_url:`${process.env.SERVER_URL}/cancel`
-  })
-res.json({ url: session.url });
-  
-
-}catch(e){
-  res.status(500).json({error:e.message})
-}
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "usd",
+            },
+            display_name: "Next day air",
+            // Delivers in exactly 1 business day
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+      line_items: req.body.cart.map((item) => {
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: item.name,
+            },
+            unit_amount: item.price * 100,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.SERVER_URL}/success`,
+      cancel_url: `${process.env.SERVER_URL}/cancel`,
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
+
+app.post(
+  "/webhook",
+  express.json({ type: "application/json" }),
+  async (req, res) => {
+    let data;
+    let eventType;
+
+    // Check if webhook signing is configured.
+    let webhookSecret;
+    //webhookSecret = process.env.STRIPE_WEB_HOOK;
+
+    if (webhookSecret) {
+      // Retrieve the event by verifying the signature using the raw body and secret.
+      let event;
+      let signature = req.headers["stripe-signature"];
+
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          signature,
+          webhookSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed:  ${err}`);
+        return res.sendStatus(400);
+      }
+      // Extract the object from the event.
+      data = event.data.object;
+      eventType = event.type;
+    } else {
+      // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+      // retrieve the event data directly from the request body.
+      data = req.body.data.object;
+      eventType = req.body.type;
+    }
+
+    // Handle the checkout.session.completed event
+    if (eventType === "checkout.session.completed") {
+        console.log(data)
+      res.status(200).end();
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
